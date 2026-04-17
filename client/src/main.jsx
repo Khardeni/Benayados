7ab434bb9fce93977543d07b4c1680e4015cfc4b
Khardeni <82120858+Khardeni@users.noreply.github.com>
2026-04-17 13:05:07 +0100

    fix: correct casing for RtlWrapper import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './i18n/index.js'   
-import RtlWrapper from './components/RtlWrapper'
 import './index.css'
-
+import RtlWrapper from './components/RtlWrapper'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RtlWrapper>
